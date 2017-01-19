47e3830a09b37cd57aa77cc4e2d4acd0e5e8b29d
Riosfen <Riosfen@gmail.com>
2017-01-19 01:07:23 +0100

    drfg

--- Relacion3Ejercicio2/src/principal/Principal.java
+++ Relacion3Ejercicio2/src/principal/Principal.java
@@ -1,64 +1,75 @@
 package principal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.io.File;
+import java.io.IOException;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Principal {
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-	private static final String URL_BD = "jdbc:sqlite:bd/ejemplo.bd";
-	private static final String DRIVER_SQLITE = "org.sqlite.JDBC";
+public class Principal {
 	
 	public static void main(String[] args) {
 		
-		Connection conexion = null;
-
-		try{
-			// para cargar el driver que se va a utilizar para la conexion
-			Class.forName(DRIVER_SQLITE);
-			
-			// para cargar la bd
-			conexion = DriverManager.getConnection(URL_BD);
-			// para que no cargue en la bd los datos automaticamente
-			conexion.setAutoCommit(false);
-			
-			java.sql.Statement sentencia = conexion.createStatement();
-			
-			sentencia.close();
-			conexion.commit();
-			conexion.close();
-			
-			ResultSet resul = sentencia.executeQuery("select * from notas");
-			
-		} catch (SQLException e) {
-			
-			System.err.println(e.getMessage());
-			System.out.println("ERROR, NO SE EFECTUARA NINGUN CAMBIO");
-			if (conexion != null) {
-				realizarRollBack(conexion);
-			}
-			
-		} catch (ClassNotFoundException e) {
-			
-			System.out.println(e.getMessage());
-			
-		}
+		AccesoBDSh2 bdH2 = null;
 		
-	}
-
-	private static void realizarRollBack(Connection conexion) {
-
 		try {
 			
-			conexion.rollback();
+			AccesoBDSqlite bdSqlite = new AccesoBDSqlite();
+			bdSqlite.crearXML("departamentos.xml", "SELECT * FROM departamentos");
+			bdSqlite.cerrarConexion();
 			
+			bdH2 = new AccesoBDSh2();
+			bdH2.ejecutarSentencia("SELECT * FROM departamento");
+			PreparedStatement sentencia = bdH2.getConexion().prepareStatement("INSERT INTO departamento VALUES(?,?,?)");
+			
+			SAXParserFactory factoria = SAXParserFactory.newInstance();
+			SAXParser parser = factoria.newSAXParser();
+			DefaultHandler oyente = new Oyente(sentencia);
+			parser.parse(new File("departamentos.xml"), oyente);
+			
+			bdH2.cerrarConexion();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (SQLException e) {
-
-			System.out.println("ERROR AL REALIZAR EL ROLLBACK");
-
+			// TODO Auto-generated catch block
+			try {
+				bdH2.getConexion().prepareStatement("CREATE TABLE departamento (codigo INTEGER(11) NOT NULL,nombre VARCHAR(50),localidad VARCHAR(50))").execute();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("ERROR AL CREAR LA TABLA 'departamento'");
+				e1.printStackTrace();
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	
 		
+	
 	}
-
 }
